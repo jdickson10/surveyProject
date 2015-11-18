6927447d873df14e74f2fee381b8deb1309424f2
jdickson10 <jdickson10@patriots.uttyler.edu>
2015-11-17 20:00:38 -0600

    end of class 11/17/2015

--- survey.R
+++ survey.R
@@ -15,17 +15,42 @@ surveys <- read.csv('data/portal_data_joined.csv')
 # weight/species vs year
 # weight/species/sex vs year
 # species vs weight
-# Species vs hindfoot_length
+# 
 # number of species vs year
 # number of sex vs month
 # sex vs weight
 # hindfoot_length vs sex
 
-## explore distribution of weight for each species
+## DM Species vs hindfoot_length
 
 # data parsing step
 
+# filter surveys and select column
+speciesDM <- surveys %>%
+  filter(species_id == "DM") %>%
+  select(hindfoot_length) %>%
+  filter(!is.na(hindfoot_length))
+
+
 # build figure
+qplot(hindfoot_length, data=speciesDM, ylab = "Frequency", xlab = "Hindfoot Length", binwidth = 1, geom="histogram")
+
+
+# filter surveys and select columns
+allspecies <- surveys %>%
+  select(hindfoot_length, species) %>%
+  filter(!is.na(hindfoot_length)) %>%
+  filter(!is.na(species))
+
+allspecies %>%
+  group_by(species) 
+
+avg <- aggregate(allspecies$hindfoot_length, list(species = allspecies$species), mean)
+  
+ggplot(data=avg, aes(x=species, y=x, fill=species)) + geom_bar(stat="identity") + scale_fill_grey()
+
+
+# make figure of average weight/species
 
 
 ## composition of data in sampling
