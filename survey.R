55dc6e64d0363f9cda2c18c79a8be707d5490ff4
jdickson10 <jdickson10@patriots.uttyler.edu>
2015-11-18 12:03:48 -0600

    second commit 11/18/205

--- survey.R
+++ survey.R
@@ -33,27 +33,57 @@ speciesDM <- surveys %>%
 
 
 # build figure
-qplot(hindfoot_length, data=speciesDM, ylab = "Frequency", xlab = "Hindfoot Length", binwidth = 1, geom="histogram")
+pdf("figures/histogram.pdf")
+qplot(hindfoot_length, data=speciesDM, ylab = "Frequency", xlab = "Hindfoot Length (mm)", binwidth = 1, geom="histogram")
+dev.off()
 
-
+## mean weight per species
 # filter surveys and select columns
 allspecies <- surveys %>%
-  select(hindfoot_length, species) %>%
-  filter(!is.na(hindfoot_length)) %>%
+  select(weight, species) %>%
+  filter(!is.na(weight)) %>%
   filter(!is.na(species))
 
+# creating a group
 allspecies %>%
   group_by(species) 
 
-avg <- aggregate(allspecies$hindfoot_length, list(species = allspecies$species), mean)
+# creating dataset with average weight per species
+avg <- aggregate(allspecies$weight, list(species = allspecies$species), mean)
   
-ggplot(data=avg, aes(x=species, y=x, fill=species)) + geom_bar(stat="identity") + scale_fill_grey()
+# building bar graph 
+pdf("figures/bargraph.pdf")
+ggplot(data=avg, aes(x=species, y=x)) + xlab("Species") + ylab("Mean Weight (g)") + geom_bar(stat="identity") + coord_flip()
+dev.off()
+
+## weight albigula vs weightmerriami
+# filter and select columns
+eremicus <- surveys %>%
+  filter(species == "eremicus") %>%
+  select(species, weight) %>%
+  filter(!is.na(weight))
+
+# filter and select for 2002
+merriami <- surveys %>%
+  filter(species == "merriami") %>%
+  select(species, weight) %>%
+  filter(!is.na(weight))
 
 
-# make figure of average weight/species
+# create combined object
+combine <- rbind(eremicus, merriami)
 
 
-## composition of data in sampling
+# create box plot
+pdf("figures/boxplot.pdf")
+ggplot(data = combine, aes(x=species, y=weight, fill=species)) + xlab("Species") + ylab("Weight (g)") + geom_boxplot() + scale_fill_discrete(name="p-value 2.2e-16")
+dev.off()
+
+# statistical test 
+t.test(weight ~ species, data=combine)
 
 
-## explore comparisons between variables
+
+
+
+
